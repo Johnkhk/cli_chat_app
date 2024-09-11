af3f2f00aa0ac75a2776ed6800698efcea9ae4fd
johnkhk <khh019@ucsd.edu>
2024-09-11 01:59:08 -0700

    sequential testing checkpoint

--- test/README.md
+++ test/README.md
@@ -1,3 +1,6 @@
 # Test by package
 this will run all tests in the package
 `go test ./test/rpc/... -v`
+
+# Run sequentially: Use this for now
+`go test -p 1 ./test/... -v`
--- test/rpc/auth_test.go
+++ test/rpc/auth_test.go
@@ -3,18 +3,20 @@ package rpc
 import (
 	"database/sql"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"google.golang.org/grpc"
 
+	"github.com/johnkhk/cli_chat_app/client/app"
 	"github.com/johnkhk/cli_chat_app/genproto/auth"
 	"github.com/johnkhk/cli_chat_app/server/logger"
 	"github.com/johnkhk/cli_chat_app/test/setup"
 )
 
 var (
-	conn   *grpc.ClientConn
-	client auth.AuthServiceClient
+	conn       *grpc.ClientConn
+	authClient *app.AuthClient
 )
 
 func TestMain(m *testing.M) {
@@ -31,26 +33,50 @@ func TestMain(m *testing.M) {
 		}
 	}()
 
-	// Initialize the gRPC client
-	client = auth.NewAuthServiceClient(conn)
+	// Initialize the token manager (replace with your actual token manager initialization)
+	filePath := filepath.Join(os.Getenv("HOME"), ".test_cli_chat_app", "jwt_tokens") // For Linux/macOS
+	tokenManager := app.NewTokenManager(filePath, nil)
+
+	// Initialize the auth client with the shared connection
+	authClient = &app.AuthClient{
+		Client:       auth.NewAuthServiceClient(conn),
+		Logger:       log,
+		TokenManager: tokenManager,
+	}
+	tokenManager.SetClient(authClient.Client)
 
 	// Run all tests
 	code := m.Run()
 	os.Exit(code)
 }
 
-func TestAuthFunctionality(t *testing.T) {
-	// Use the initialized gRPC client to make a call
-	// req := &auth.LoginRequest{Username: "testuser", Password: "testpassword"}
+// Test the new user registration
+func TestNewUserRegister(t *testing.T) {
 
-	// // Call the Login method on the client
-	// resp, err := client.Login(context.Background(), req)
-	// if err != nil {
-	// 	t.Fatalf("Login failed: %v", err)
-	// }
+	// clear the directory
+	authClient.Logger.Infof("Removing test directory")
+	if err := os.RemoveAll(filepath.Join(os.Getenv("HOME"), ".test_cli_chat_app")); err != nil {
+		t.Fatalf("Failed to remove test directory: %v", err)
+	}
 
-	// // Check the response
-	// if resp.GetToken() == "" {
-	// 	t.Errorf("Expected a token, got an empty string")
-	// }
+	// Test the new user registration
+	authClient.Logger.Infof("Testing new user registration")
+	err := authClient.RegisterUser("testuser", "testpassword")
+	if err != nil {
+		t.Fatalf("Failed to register user: %v", err)
+	}
+
+	// Make sure the directory was not created (directory gets created on login)
+	authClient.Logger.Infof("Checking test directory")
+	if _, err := os.Stat(filepath.Join(os.Getenv("HOME"), ".test_cli_chat_app")); err == nil {
+		t.Fatalf("Test directory was created")
+	}
+
+}
+
+// Test the login of an unregistered user
+func TestUnregisteredUserLogin(t *testing.T) {
+	authClient.Logger.Infof("Testing unregistered user login")
+
+	// Attempt to login with an unregistered user
 }
