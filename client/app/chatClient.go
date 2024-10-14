f020587daf69ef58ea2d9b2244525d676e5b74bb
johnkhk <khh019@ucsd.edu>
2024-10-13 20:01:12 -0700

    do not send to channel if stored server buffer

--- client/app/chatClient.go
+++ client/app/chatClient.go
@@ -277,6 +277,7 @@ func (cc *ChatClient) listenForMessages(ctx context.Context) {
 				cc.Logger.Infof("User Connected at %s", resp.Timestamp)
 			case "stored":
 				cc.Logger.Infof("Message %s was stored in server buffer for later delivery at %s", resp.MessageId, resp.Timestamp)
+				continue
 			default:
 				cc.Logger.Warnf("Unknown response type: %s", resp.Status)
 			}
